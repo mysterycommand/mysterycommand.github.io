64c159111c2cc99a77591f0b6cea52ec28ddfd42
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-01 06:07:37 +0000

    Deploy mysterycommand.com to github.com/mysterycommand/mysterycommand.github.io.git:master

---
+++ precache-manifest.8a5dfb701dcc35a9557f80cbd2bc5b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ca6a35533ec268b37f8346e2a75cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8074a3e54ee0579568ad",
+    "url": "/static/css/main.97f572c1.chunk.css"
+  },
+  {
+    "revision": "5f99e5d98a7f5c0f6dc1",
+    "url": "/static/js/2.9895c38b.chunk.js"
+  },
+  {
+    "revision": "605f18dc9cc76767df2a",
+    "url": "/static/js/3.caba9ec0.chunk.js"
+  },
+  {
+    "revision": "4ac4e6cadad71224ac09",
+    "url": "/static/js/4.0756bbeb.chunk.js"
+  },
+  {
+    "revision": "8074a3e54ee0579568ad",
+    "url": "/static/js/main.2fdc7e0e.chunk.js"
+  },
+  {
+    "revision": "28b454b10264412f39bd",
+    "url": "/static/js/runtime~main.0836c9ed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a9d6a965c33d034b584d821d4806af2.js"
+  "/precache-manifest.8a5dfb701dcc35a9557f80cbd2bc5b21.js"
 );
 
 self.addEventListener('message', (event) => {
