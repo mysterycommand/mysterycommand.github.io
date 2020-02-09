aa109e4c347848a17c76730d47d9469744227a93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-09 22:05:07 +0000

    Deploy mysterycommand.com to github.com/mysterycommand/mysterycommand.github.io.git:master

---
+++ precache-manifest.09a96d493e581c4c1384df91dd324ff8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55baa129332dda3ce74979136e5d32c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb33e476981ae1bcbb6b",
+    "url": "/static/css/main.e61ae6dd.chunk.css"
+  },
+  {
+    "revision": "d9f4ca6832daea152cb0",
+    "url": "/static/js/2.6a313f92.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/static/js/2.6a313f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff917f085fcce44b2627",
+    "url": "/static/js/3.bba2d84a.chunk.js"
+  },
+  {
+    "revision": "bb7007258c0ce855ed01f5b568122c76",
+    "url": "/static/js/3.bba2d84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e29982d680477150885",
+    "url": "/static/js/4.f226a256.chunk.js"
+  },
+  {
+    "revision": "bb33e476981ae1bcbb6b",
+    "url": "/static/js/main.079df00b.chunk.js"
+  },
+  {
+    "revision": "89f6d9f01c75710dfa3a",
+    "url": "/static/js/runtime-main.3039c01a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6acd14facc3f91b4f9d0da5286104e44.js"
+  "/precache-manifest.09a96d493e581c4c1384df91dd324ff8.js"
 );
 
 self.addEventListener('message', (event) => {
